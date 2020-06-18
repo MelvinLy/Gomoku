f11222d358448f64dc6636610810af891f8af003
MelvinLy <melvinly@gmail.com>
2020-06-18 18:09:11 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,3 +1,21 @@
 public class Board {
+	final static int SIZE = 19;
+	private char[][] arrayRep = new char[SIZE][SIZE];
+	
+	public boolean setChar(int row, int col, char val) {
+		if(val != 'X' || val != 'O') {
+			return false;
+		}
+		if(row >= 0 && row <= 18 && col >= 0 && col <= 18) {
+			if(arrayRep[row][col] != 0) {
+				return false;
+			}
+			arrayRep[row][col] = val;
+			return true;
+		}
+		return false;
+	}
+	
+	
 	
 }

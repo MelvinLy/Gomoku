6e1491949b42dfe16442d7a38bc548d0e8ad830a
MelvinLy <melvinly@gmail.com>
2020-06-19 22:49:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,16 +1,31 @@
+import java.io.IOException;
+import java.net.*;
+
+
 public class Player {
+	private DatagramSocket udp;
+	private InetAddress address;
+	private int port;
+	private byte[] buf;
 	
-	private static int SIZE;
-	private static int WINSIZE;
-	public char[][] arrayRep;
-	
-	private Player() {};
-	
-	private Player(int size, int winsize) {
-		this.SIZE = size;
-		this.WINSIZE = winsize;
-		arrayRep = new char[SIZE][SIZE];
+	public Player(int port, byte[] outAddress) throws SocketException, UnknownHostException {
+		udp = new DatagramSocket();
+		this.port = port;
+		address = InetAddress.getByAddress(outAddress);
 	}
 	
+	public void send(String message) throws IOException {
+		byte[] out = message.getBytes();
+		DatagramPacket packet = new DatagramPacket(out, out.length, address, port);
+		udp.send(packet);
+	}
 	
+	public String receive() throws IOException {
+		byte[] receive = new byte[576];
+		DatagramPacket in = new DatagramPacket(receive, receive.length);
+		udp.receive(in);
+		byte[] received = in.getData();
+		String out = new String(received, 0, received.length);
+		return out;
+	}
 }

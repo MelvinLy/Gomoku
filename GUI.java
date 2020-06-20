551b860a26eef419f792ffbf1474ec72faeb5ce1
MelvinLy <melvinly@gmail.com>
2020-06-20 00:07:30 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,19 +1,28 @@
-import java.awt.Dimension;
-
+import java.awt.GridLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI {
 	
 	private Board board;
 	private Player player;
 	private JFrame frame;
+	private JPanel panel;
 	
 	public GUI() {
 		this.board = new Board();
 		this.frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 600);
-		frame.setVisible(true); 
+		createGridLayout();
+		frame.setVisible(true);
+	}
+	
+	public void createGridLayout() {
+		this.panel = new JPanel();
+		this.panel.setLayout(new GridLayout(Board.SIZE, Board.SIZE));
+		this.frame.add(panel);
+		//Add some buttons in array to be accessed
 	}
 	
 	public static void main(String args[]) {

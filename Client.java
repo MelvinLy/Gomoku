d4cd5c3b585fd291dd2d2c7d5ebbe249efc8880e
MelvinLy <melvinly@gmail.com>
2020-06-19 23:33:41 -0400

    Talky talk between the server and client.

---
+++ Client.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.util.Scanner;
+import java.io.*; 
+
+public class Client {
+	private Socket clientSocket;
+
+	public Client(String name, int port) throws Exception {
+		this.clientSocket = new Socket(name, port);
+	}
+
+	public void send(String message) throws Exception {
+		DataOutputStream outServer = new DataOutputStream(clientSocket.getOutputStream());
+		outServer.writeBytes(message + "\n");
+	}
+
+	public String receive() throws Exception {
+		BufferedReader inServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		String sentenceIn = inServer.readLine();
+		return sentenceIn;
+	}
+
+	public void close() throws Exception{
+		clientSocket.close();
+	}
+
+
+	
+	public static void main(String[] args) throws Exception {
+		Client client = new Client("localhost", 56789);
+		Scanner s = new Scanner(System.in);
+		String message = s.nextLine();
+		client.send(message);
+		System.out.println(client.receive());
+		client.close();
+	}
+	
+}
--- Server.java
+++ Server.java
@@ -1,3 +1,41 @@
+import java.net.*;
+import java.io.*;
+
 public class Server {
+	private ServerSocket serverSocket;
+    private Socket connectionSocket;
+    private int port;
+    
+    public Server(int port) throws IOException {
+    	this.port = port;
+    	serverSocket = new ServerSocket(port);
+    }
+    
+	public String listen() throws IOException {
+		this.connectionSocket = serverSocket.accept();
+		String clientSentence;
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+		clientSentence = inFromClient.readLine();
+		return clientSentence;
+	}
+    
+	public void send(String message) throws Exception {
+		DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+		outToClient.writeBytes(message + "\n");
+	}
+	
+	public void close() throws IOException {
+		this.serverSocket.close();
+		this.connectionSocket.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(56789);
+		while(true) {
+			String r = server.listen();
+			server.send(r);
+			System.out.println(r);
+		}
+	}
 	
 }
